d7a3825882627670b4abf9b4ad12637fd5a02dbd
Macdonald-Prince-Yingura <princeyingura@gmail.com>
2025-08-01 12:56:55 +0000

    Add W01 Home Page Assignment

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -11,3 +11,5 @@ document.addEventListener("DOMContentLoaded", () => {
     modPara.textContent = `Last Modified: ${document.lastModified}`;
   }
 });
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
