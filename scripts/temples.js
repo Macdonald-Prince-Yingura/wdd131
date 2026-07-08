936dc5e190b2de5e8cb2114287ba574784857565
Macdonald-Prince-Yingura <you@example.com>
2026-07-08 13:49:38 +0000

    week 2-temples

---
+++ scripts/temples.js
@@ -0,0 +1,19 @@
+const year = document.querySelector("#year");
+
+year.textContent = new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+`Last Modified: ${document.lastModified}`;
+
+const menu = document.querySelector("#menu");
+
+const nav = document.querySelector(".navigation");
+
+menu.addEventListener("click", () => {
+
+nav.classList.toggle("open");
+
+menu.textContent =
+nav.classList.contains("open") ? "✖" : "☰";
+
+});
